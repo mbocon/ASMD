f605b702479e3df9fce2e743e4396ac3ea3b4467
Michael Bocon <mikebocon@gmail.com>
2020-08-26 22:13:24 -1000

    mod test page

--- client/src/Test.js
+++ client/src/Test.js
@@ -7,8 +7,8 @@ const Test = () => {
 		<div>
 			<Navbar />
 			<div className='study'>
-                <h4>This page is still under construction</h4>
-                <h6>Select something to study in the meantime</h6>
+                <h4>Page under construction...</h4>
+                <h6 className='practice-h6'>In the meantime, select something to do.</h6>
 				<div className='study-selection'>
 					<a href='/add' className='btn btn-danger select-btn'><span className='add-btn'>Add</span> <i className="fa fa-plus"></i></a>
 					<a href='/subtract' className='btn btn-info select-btn'><span className="subtract-btn">Subtract</span><i className="fa fa-minus"></i></a>
